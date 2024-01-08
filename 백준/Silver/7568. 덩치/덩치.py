859fc38bf1fb524a7070075b5ad1ab689ba72024
Jaekyeun Joe <93318615+JamesJoe0830@users.noreply.github.com>
2024-01-08 13:41:28 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/7568
+# 7568. 덩치 [🥈 실버5]
+# 📚 알고리즘 분류: 브루트 포스 [Brute Force 알고리즘]
+# ⏰ 걸린 시간 : 5분
+# 시간복잡도 : O(N^2)
+# [문제 풀이]
+# 0. 몸무게, 키 모두 작다면 순위를 하나씩 증가해준다.
+# 1. 그 이후 100 이상 1000 미만은 0번째 1 번째 차이와 1번째 2번째 차이를 같다면 
+# ----------------------------------------------------------------
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+people = []
+for i in range(N):
+    w, l = map(int, input().split())
+    people.append((w,l))
+answer = []
+for i in range(N):
+    rank = 1
+    for j in range(N):
+        if i != j and people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+            rank +=1
+        
+    answer.append(rank)
+print(*answer)
+# ----------------------------------------------------------------
