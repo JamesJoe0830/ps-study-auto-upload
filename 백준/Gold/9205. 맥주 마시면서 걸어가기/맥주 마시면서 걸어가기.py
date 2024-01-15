f90b8c9c16bcd609bc7f74c020eb01136799e6e4
Jaekyeun Joe <93318615+JamesJoe0830@users.noreply.github.com>
2024-01-15 16:51:19 +0900

    Feat : BFS&DFS(by BFS)

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,9 +1,14 @@
 # https://www.acmicpc.net/problem/9205
-# 
-# 
-# 
-# 편의점, 상근이네 집, 펜타포트 락 페티벌
-# 편의점에 도착하면 맥주 20개로 리셋
+# 9205. 맥주 마시면서 걸어가기  [🥇 골드 5]
+# 📚 알고리즘 분류: 큐 (Que)
+# ⏰ 걸린 시간 : 20분
+# [문제 풀이 방법]
+# 0. BFS로 접근해서 푼다.
+# 1. Q에 (home_x,home_y)를 넣고 시작한다.
+# 2. "편의점에 도착하면 맥주 20개로 리셋"은 사실상 무의미하다. q에서 1000(20개*50m) 보다 작다면
+# 3. 바로바로 bfs탐색을 해서 now_x,now_y를 업데이트 해주기 때문에 
+# 4. while문에서 목적지의 x, y값과 festival의 x, y값의 차이를 계산해서 합한 값이 1000보다 작다면 도달할 수 있는다는 것이고 이를 확인해주면 된다.
+# --------------------------------------------------------------------------------------------------------------------
 
 import sys
 from collections import deque
@@ -26,15 +31,17 @@ for test_case in range(t):
     festival_x,festival_y = map(int,input().split())
     while q:
         now_x,now_y = q.popleft()
-        if abs(festival_x - now_x) + abs(festival_y - now_y) <= 1000:
+        # 현재 시점이 목적지에 바로 갈 수 있는 거리라면 바로 happy 출력
+        if abs(festival_x - now_x) + abs(festival_y - now_y) <= 1000: 
             answer = "happy"
             break
         if store_q:
             for i in range(len(store_q)):
                 next_x = store_q[i][0]
                 next_y = store_q[i][1]
-                # print(next_x != now_x and next_y!=now_y and (next_x - now_x) + (next_y - now_y) <= 1000)
                 if abs(next_x - now_x) + abs(next_y - now_y) <= 1000 and i not in visited:
                     visited.add(i)
                     q.append((next_x,next_y))
     print(answer)
+
+# --------------------------------------------------------------------------------------------------------------------
