3176966407b9cde219d5b1a03bc83ab623bcb58b
Jaekyeun Joe <93318615+JamesJoe0830@users.noreply.github.com>
2024-01-17 14:53:23 +0900

    Feat : BFS (layer문제)
    layer문제는 BFS가 더 적합하다.

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,40 +1,39 @@
+# https://www.acmicpc.net/problem/7562
+# 7562. 나이트의 이동 [🥈 실버 1] 
+# 📚 알고리즘 분류: DFS&BFS (by BFS)
+# ⏰ 걸린 시간 :25분
+# 
+# [문제 풀이] : BFS 근거
+# 1. 나이트는 결국 몇번째 만에 도달했는지 최단 거리를 구하고 싶다. -> 최소 깊이를 알고싶음
+# 2. layer(계층)을 접근하는 문제는 DFS에 적합하지 않다. BFS에 적합하다.
+# ----------------------------------------------------------------------- 
+
 import sys
-input = sys.stdin.readline
 from collections import deque
+input = sys.stdin.readline
 t = int(input())
 
-
-dx = [-1,1,-1,1,2,-2,2,-2]
-dy = [-2,-2,2,2,1,1,-1,-1]
-answer = []
-
-def BFS():
-    Que.append((str_x,str_y))
-    while Que:
-        (x,y) = Que.popleft()
-        if (end_x,end_y) == (x,y):
-            answer.append(graph[x][y])
+for test_case in range(t):
+    l = int(input())
+    start_y,start_x = map(int,input().split())
+    end_y, end_x = map(int,input().split())
+    graph = [[0]*l for i in range(l)]
+    dx = [-2,-2,2,2,-1,-1,1,1]
+    dy = [1,-1,1,-1,-2,2,2,-2]
+    q=deque()
+    q.append((start_y,start_x,0))
+    graph[start_y][start_x] = 1
+    #bfs
+    while q:
+        cur_y,cur_x,cnt = q.popleft()
+        if cur_y == end_y and cur_x == end_x:
+            print(cnt)
             break
-
         for i in range(8):
-            nx_x = x + dx[i]
-            nx_y = y + dy[i]
-            if N > nx_x >= 0 and N > nx_y >=0 and graph[nx_x][nx_y] ==0 :
-                graph[nx_x][nx_y] = graph[x][y] + 1
-                Que.append((nx_x,nx_y))
-            
-for _ in range(t):
-    N = int(input())
-    graph = [[0]*N for _ in range(N)]
-    str_x,str_y = map(int,input().split())
-    end_x,end_y = map(int,input().split())
-    graph[str_x][str_y] = 1
+            next_y = cur_y + dx[i]
+            next_x = cur_x + dy[i]
+            if 0 <= next_y < l and 0 <= next_x < l and graph[next_y][next_x] == 0:
+                graph[next_y][next_x] = 1 # visited(방문체크)
+                q.append((next_y, next_x,cnt+1))
 
-    Que = deque()
-    BFS()
-
-
-
-# 출력
-for i in range(t):
-    print(answer[i]-1)
+# ----------------------------------------------------------------------- 
