c596ef329fae65d4a476e4eb9ee8811aadd0deea
Jaekyeun Joe <93318615+JamesJoe0830@users.noreply.github.com>
2024-02-01 15:30:27 +0900

    Feat : 스택(Stack) 🔥
    🔥 오답 : 스택(Stack) 좋은 문제

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,3 +1,15 @@
+# https://www.acmicpc.net/problem/9935
+# 9935. 문자열 폭발 [🥇 골드4] 🔥 오답 필요
+# 📚 알고리즘 분류: 스택 [STACK]
+# ⏰ 걸린 시간 : 23분
+# 
+# [문제 풀이]
+# 0. stack에 집어 넣는다. 
+# 1. stack에 넣을때 마지막 값이 boom의 마지막값과 일치하다면
+# 2. 여태 넣은 값의 뒤에 값을 boom하고 비교하고 정확하게 일치하다면
+# 3. stack 에서 boom길이 만큼 빼준다.
+# ---------------------------------------------------------------- 
+
 import sys
 input = sys.stdin.readline
 
@@ -12,8 +24,10 @@ for i in word:
             for i in range(len(boom)):
                 stack.pop()
         
-
+# 출력
 if stack :
     print("".join(stack))
 else:
     print("FRULA")
+
+# ---------------------------------------------------------------- 
