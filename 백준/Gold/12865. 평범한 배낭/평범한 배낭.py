8c8828eb41cf35e506ab33c526d917a8224a0dff
Jaekyeun Joe <93318615+JamesJoe0830@users.noreply.github.com>
2024-07-02 17:33:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 3068 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,25 +1,21 @@
 # https://www.acmicpc.net/problem/12865
 import sys
 input = sys.stdin.readline
-
-# N : 물품의 수 K: 버틸 수 있는 무게
-N, K = map(int, input().split())
-arr = [[0,0]]
-for i in range(N):
-    # W : 물건의 무게, # V: 물건의 가치
-    W, V = map(int, input().split())
-    arr.append((W, V))
-
-dp = [[0]*(K+1) for _ in range(N+1)]
-
-for i in range(1,N+1):    
-    for j in range(i,K+1):
-        weight = arr[i][0]
-        value = arr[i][1]
-
-        if weight <= j:         
-            dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight]+value)
-        else:
-            dp[i][j] = dp[i-1][j]
-
-print(dp[-1][K])
+def knapsack():
+    N, K = map(int, input().split())
+    arr =[]
+    dp = [[0]*(K+1) for _ in range(N+1)]
+    for _ in range(N):
+        W, V = map(int, input().split())
+        arr.append((W,V))
+    for i in range(1,N+1):
+        for j in range(i,K+1):
+            weight = arr[i-1][0]
+            value = arr[i-1][1]
+    # 무게를 넘지 않으면서 최대로 만들 수 있는 가치를 측정하는 것이다.
+            if j >= weight:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight]+value)
+            else : 
+                dp[i][j] = dp[i-1][j]
+    return dp[-1][K]
+print(knapsack())
